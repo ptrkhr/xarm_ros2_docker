533151a83dad1e53c79c16c1245733b8b124b3e0
Petr Khrapchenkov <petr.khrapchenkov@gmail.com>
2024-10-29 17:23:33 +0900

    chore!: add of compose file and modify the docker file

--- BUILD-DOCKER-IMAGE.sh
+++ BUILD-DOCKER-IMAGE.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build . -f docker/Dockerfile.humble_cuda12 -t ros:humble-cuda12
+# WARNING: the main assumption here is that humble image is going to be 
+# built separately. Temporarily, leave the direct docker command.
 
-docker build . --ssh defalut -f docker/Dockerfile.xarm_humble -t matsuolab/xarm:humble
+docker build . -f docker/Dockerfile.humble_cuda12 -t ros:humble-cuda12
+docker compose build
